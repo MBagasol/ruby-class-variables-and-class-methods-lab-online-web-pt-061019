00b961e464c9728f0a3261b4ba50108bc43e49a5
Mark Anthony Bagasol <markbagasol@gmail.com>
2019-07-04 06:14:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,8 +26,11 @@ class Song
     artist_count = {}
     artist_count.each do |artist|
       if artist_count[artist] 
-        artist_count[artist]+=1 
+        artist_count[artist] += 1 
       else 
-        
+        artist_count[artist] = 1
+      end
+      artist_count
+    end
       
 end
